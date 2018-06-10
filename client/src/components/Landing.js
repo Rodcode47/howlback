0cfb085e769d93970f5dd5421a262cc0b50cb438
rodcode47 <rodney.hammad@gmail.com>
2018-06-10 12:04:43 +0300

    Updated the client UI

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,7 +7,8 @@ const Landing = () => {
     <div>
       <hr/>
         <h2 className="headertext text-monospace"><u>Howlback!</u></h2><br/>
-          <h5 className="subheadertext">Collect feedback form your clients Only at <small className="fa fa-money"> $5 </small> For 8 Credits!</h5>
+          <h5 className="subheadertext text-info">Collect feedback from your clients, 
+            <i className="text-secondary"> easy</i> and <i className="text-secondary">fast</i> Only at <i className="text-danger">$5</i> for <b>8</b> Credits!</h5>
       <hr/>
       <div className="alert alert-success alert-dismissible fade show" role="alert">
         <button type="button" className="close" data-dismiss="alert" aria-label="Close" title="Close This Ad">
@@ -20,10 +21,10 @@ const Landing = () => {
             </a>
           </div>
           <div className="p-2">
-            <h5 className="alert-heading" title="Visit Our Blog"><a href="https://kodeflash.herokuapp.com/blogs" target="_new">[#1 Full-Stack] React and Redux</a></h5>
+            <h5 className="alert-heading" title="Visit Our Blog"><a href="https://kodeflash.herokuapp.com/blogs" target="_new"><b>[#1 Full-Stack] React and Redux</b></a></h5>
             <p>
               Master React and Redux! by building a massive and awesome web application. We will use advanced features of React, 
-              Redux, Express, and Mongo. By putting each concept into a fullstack web application. Other technologies feature like 
+              Redux, Express, and MongoDB. By putting each concept into a full-stack web application. Other technologies feature like 
               SendGrid (For Handling Email), and Stripe (For Billing/Payments) are used in this application.
             </p>
             <ul>
@@ -38,11 +39,11 @@ const Landing = () => {
         <div className="jumbotron">
           <h1>Welcome To Feedback</h1>
           <p className="lead">
-            Do you want to send one survey to thousand of your clients and get feedback, great your in 
-            the right place. Howlback lets you create surveys that measures your clients opinions or 
-            experiences through your question.
+            Do you want to <b>send a survey to your clients and get feedback</b>, easy and fast great your in 
+            the right place. Howlback you will create surveys that measures your clients opinions or 
+            experiences through your question. <b>One click send to thousands of your Clients</b>. <i className="text-info">YES 1 Click</i>
           </p>
-          <p>Our goal is to creat great communication between our clients and there clients with easy</p>
+          <p className="text-danger">Our goal is to create great medium that communication flawlessly with you and your clients.</p>
           <a className="btn btn-lg btn-primary" href="/surveys" role="button" title="View All Your Surveys">View Your Surveys &raquo;</a>
         </div>
       </main>
