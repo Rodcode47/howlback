365ec3aaeeb5f9b6973a8483c0871c5504c8f940
Rodney Hammad <rodney.hammad@gmail.com>
2020-12-07 12:08:19 +0200

    fix navbar buttons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,11 +9,13 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return
-          <li>Login With &raquo;
-            <a className='dropdwon-button btn fab fa-google' href="/auth/google" title="Login With Google"> Google</a>Or
-            <a className='dropdwon-button btn fab fa-linkedin' href="/auth/linkedin" title="Login With Linkedin"> Linkedin</a>
-          </li>;
+        return (
+          <li>
+            <span className="h5">Login With:</span>
+            <a className='dropdwon-button btn fab fa-google' href="/auth/google" title="Login With Google"> Google</a>
+            <a className='dropdwon-button btn fab fa-linkedin' href="/auth/linkedin" title="Login With Linkedin"> LinkedIn</a>
+          </li>
+        )
       default:
         return [
           <li key="2"><a className='dropdwon-button btn fas fa-sign-out-alt' href="/api/logout" title="Logout"> Logout</a></li>
@@ -25,7 +27,7 @@ class Header extends Component {
     return (
       <li>
         <a className='dropdwon-button btn' href="/" title="Home">
-          <img src="assets/icons/home.png" alt="Home" className="mb-2" /> Home
+        <img src="assets/icons/home.png" alt="Home" className="mb-2" /> Home
         </a>
       </li>
     );
@@ -35,34 +37,29 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <ul className=" mt-2" style={{ margin: '0 0 0 0' }}>
+          <ul className="mt-4">
             {this.renderOtherLinks()}
+            {this.props.auth ?
+              <div>
+
+                <li key="3" title="Credit Count" className="ml-3 mt-1">
+                   <span className="h4">Credits: {this.props.auth.credits}</span>
+                </li>
+                <li key="1" className="ml-3"><Payments /></li>
+              </div>
+              :
+              null
+            }
           </ul>
 
           <Link
               to={this.props.auth ? '/surveys' : '/'}
-              className="left brand-logo"
+              className="left brand-logo ml-3"
           >
-            <button type="button" className="btn btn-outline-success ml-3 mb-2" title="All Feedback Surveys">
+            <button type="button" className="waves-effect waves-light btn mb-1 ml-2" title="All Feedback Surveys">
               <img src="assets/icons/script.png" alt="Feeds" /> Feeds
             </button>
           </Link>
-
-          <ul>
-            <ul className=" mt-2" style={{ margin: '0 0 0 0' }}>
-              {this.renderOtherLinks()}
-              {this.props.auth ?
-                <div>
-                  <li key="1"><Payments /></li>
-                  <li key="3" title="Credit Count" style={{ margin: '0 10px' }}>
-                    <span className="h4">Credits: {this.props.auth.credits}</span>
-                  </li>
-                </div>
-                :
-                null
-              }
-            </ul>
-          </ul>
 
           <ul className="right">
             {this.renderContent()}
